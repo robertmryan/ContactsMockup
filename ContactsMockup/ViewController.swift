278b31f50a228a47b6fe56d1e57b4ecde3eb980e
Robert Ryan <robert.ryan@mindspring.com>
2021-01-14 09:25:11 -0800

    Initial Commit

---
+++ ContactsMockup/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactsMockup
+//
+//  Created by Robert Ryan on 1/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
